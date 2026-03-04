3038604d00172041969173a09c3d0803e17cab43
SANDIPAN NAYEK <sandipannayek09@gmail.com>
2026-03-05 02:11:17 +0530

    api response and error handling

--- src/index.js
+++ src/index.js
@@ -4,8 +4,25 @@ import dotenv from "dotenv"
 dotenv.config({
     path: './.env'
 })
-
+ import express from "express";
+ const app=express();
 connectDB()
+.then(() => {
+
+     app.on("error", (error) => {
+        console.log("error",console.error
+        )
+        throw error
+        
+       })
+
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(`server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection fail" ,err)
+})
 
 
 
