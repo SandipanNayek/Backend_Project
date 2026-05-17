1d87d34135c9a48a69ad16535cc1a2bceb36c9d1
SandipanNayek <sandipannayek09@gmail.com>
2026-05-17 23:23:05 +0530

    Fix syntax error in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -14,7 +14,7 @@ export const verifyJWT = asyncHandler(async(req,_,next)=>{
    const decodeToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
   
    const user = await User.findById(decodeToken?._id).select(
-      "-password -refreshToken"
+      "-password -refreshToken";
     )
   
     if(!user){
